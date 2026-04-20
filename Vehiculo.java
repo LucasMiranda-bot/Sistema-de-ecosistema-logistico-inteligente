cc3ef6cfb641eafc70118ea21d36ca3b36f77097
Lucas Miranda <lucas.miranda@fiuni.edu.py>
2026-04-20 01:59:02 +0000

    new file:   Vehiculo.java

---
+++ Vehiculo.java
@@ -0,0 +1,14 @@
+public abstract class Vehiculo {
+    private int id;
+
+    public Vehiculo(int id){
+        this.id = id;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public abstract void patronMovimiento();
+
+}
